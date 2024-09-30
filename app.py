01ce1d0afde0361c84f5c15783dee522fd964a80
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-09-30 16:54:29 +0400

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/' , methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -11,6 +11,14 @@ def index():
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/categories', methods=['GET', 'POST'])
+def categories():
+    return render_template('categories.html')
 
 
 if __name__ == '__main__':
