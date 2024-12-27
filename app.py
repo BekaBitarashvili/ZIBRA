1ba11f6e7a54fbd46c22539d8a55e4c6b2faf00f
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-12-27 14:12:41 +0400

    Update

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ def login():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
